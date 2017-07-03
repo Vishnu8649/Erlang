f1f5234d534896ea41dee2b71051f834a31b6f26
Vishnu8649 <vishnu.mistics.t@gmail.com>
2017-07-03 19:02:15 +0530

    my first erlang program

--- second.erl
+++ second.erl
@@ -1,11 +1,11 @@
 -module(second).
 -export([hypot/2,peri/2,area/2]).
 
-hypot(X,Y)->
+hypot(X,Y)->%function to give hypotenuse
 	math:sqrt(first:sqr(X)+first:sqr(Y)).
 
-peri(X,Y)->
+peri(X,Y)->%function to give perimeter
 	X+Y+hypot(X,Y).
 
-area(X,Y)->
+area(X,Y)->%function to give area
 	0.5*X*Y.
