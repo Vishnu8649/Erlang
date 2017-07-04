e22d3fdb9d68568c429e4a91c45a529108384a14
Vishnu8649 <vishnu.mistics.t@gmail.com>
2017-07-04 19:21:20 +0530

    Fibonacci

--- fib.erl
+++ fib.erl
@@ -1,9 +1,16 @@
 -module(fib).
--export([fib/1]).
+-export([fib/1,fib1/1]).
 
-fib(1)->
+fib(0)->%direct recursion
 	0;
 fib(1)->
 	1;
 fib(N) when N>1 ->
 	fib(N-1)+fib(N-2).
+
+fib1(N)->%tail recursion
+	fib1(0,1,N).
+fib1(X,_Y,0)->
+	X;
+fib1(X,Y,I)->
+	fib1(Y,X+Y,I-1).
